95766f5d4380e1af5970684e5047e24d33d10eac
chelomh26-sudo <chelomh26@icloud.com>
2026-06-12 15:29:42 -0600

    Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// VitePWA se activa después de correr npm install
+// import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+ 
